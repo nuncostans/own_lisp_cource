a84d51997382c3355367f1e7fdbc4f7545a9c2a3
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-03-10 13:40:11 +0200

    Add instruction for win32

--- src/repl.c
+++ src/repl.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+// if win
+#ifdef _WIN32
+#include <string.h>
 
+static char buffer[2048];
+
+// fake readline
+char* readline(char* prompt)
+{
+        fputs(prompt, stdout);
+        fgets(buffer, 2048, stdin);
+        char* cpy = malloc(strlen(buffer)+1);
+        strcpy(cpy, buffer);
+        cpy[strlen(cpy)-1] = '\0';
+        return cpy;
+}
+// fake add_history function
+void add_history(char* unused){}
+
+//otherwise include the editline headers
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
+
 static char input[2048];
 
 int main(int argc, char** argv)
