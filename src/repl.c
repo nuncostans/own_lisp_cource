4286e8d769bc4df54783a6b89a5c690077ed000d
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-03-10 12:51:31 +0200

    Add first version of repl

---
+++ src/repl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+static char input[2048];
+
+int main(int argc, char** argv)
+{
+        puts("Lispy version 0.0.0.0.1");
+        puts("Press Ctrl+C to exit\n");
+
+        while(1)
+        {
+                fputs("lispy> ", stdout);
+                fgets(input, 2048, stdin);
+                printf("No you are a %s", input);
+        }
+        return 0;
+}
